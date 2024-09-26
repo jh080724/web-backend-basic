eee2370b590de7b4b41a01fd127bdeefe90a6b0e
jh080724 <tsj010813@gmail.com>
2024-09-26 16:56:51 +0900

    MVC - JDBC DB연동용 인터페이스 클래스

---
+++ src/main/java/com/spring/basic/servlet/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.spring.basic.servlet.repository;
+
+import com.spring.basic.servlet.domain.Member;
+
+import java.util.List;
+
+public interface MemberRepository {
+    void save(Member member);
+
+    List<Member> getList();
+
+}
