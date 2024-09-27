7657c66a2f50427674d153e01b68ec341e15537a
jh080724 <tsj010813@gmail.com>
2024-09-27 16:02:24 +0900

    FrontController v4 구축(FrontController가 ModelView일괄처리, 하위 컨트롤러는 viewName을 문자열로만 리턴)

---
+++ src/main/java/com/spring/basic/servlet/web/frontcontroller/v4/controller/FrontControllerServlet.java
@@ -0,0 +1,94 @@
+package com.spring.basic.servlet.web.frontcontroller.v4.controller;
+
+//import com.spring.basic.servlet.web.frontcontroller.v1.ControllerV1;
+import com.spring.basic.servlet.web.frontcontroller.Model;
+import com.spring.basic.servlet.web.frontcontroller.ModelView;
+import com.spring.basic.servlet.web.frontcontroller.v3.ControllerV3;
+import com.spring.basic.servlet.web.frontcontroller.v4.ControllerV4;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/front-controller/v4/*")
+public class FrontControllerServlet extends HttpServlet {
+
+    // 각 컨트롤러들을 모아놓을 Map생성
+    // key를 url로 주어서 각 요청마다 명령을 내릴 콘트롤러를 구분
+    private Map<String, ControllerV4> controllerMap = new HashMap<>();
+
+    // 객체 초기화시 url에 따라 사용할 컨트롤러 초기화
+    public FrontControllerServlet() {
+        controllerMap.put("/front-controller/v4/member/form", new MemberFormController());
+        controllerMap.put("/front-controller/v4/member/join", new MemberJoinController());
+        controllerMap.put("/front-controller/v4/members", new MemberListController());
+        controllerMap.put("/front-controller/v4/member/delete", new MemberDeleteController());
+
+    }
+
+    @Override  // 부모 메소드 재정의: 리턴타입 동일, 메소드 이름동일, 매개변수 동일,
+    protected void service(HttpServletRequest request,
+                           HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("[dbg] Call! FrontControllerServlet service()");
+
+        String requestURI = request.getRequestURI();
+
+        System.out.println("[dbg] " + requestURI);
+
+        // 요청 URL에 맞는 컨트롤러를 앱에서 꺼내오기
+        ControllerV4 controller = controllerMap.get(requestURI);
+
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // 요청과 함께 전달된 데이터(Request Parameter)를 전부 읽어서 맵에 담아 리턴하는 로직.
+        // ?name=Kim&age=30...
+        Map<String, String> paramMap = createParamMap(request);
+
+        // 이제는 하위 컨트롤러에게 모델을 전달해야 한다.
+        Model model = new Model();
+        String viewName = controller.process(paramMap, model);
+
+        // 각 컨트롤러가 리턴한 viewName과 전달했던 Model을
+        // ModelView에 일괄 세팅.
+        ModelView mv = new ModelView(viewName);
+        mv.setModel(model);
+
+        // jsp 쪽에서 좀 더 쉽게 데이터를 꺼낼 수 있게
+        // Model이 가진 맵을 순회해서 request 객체에 데이터를 세팅
+        Map<String, Object> modelMap = mv.getModel().getModelMap();
+        for (String key : modelMap.keySet()) {
+            request.setAttribute(key, modelMap.get(key));
+        }
+
+        // view를 랜더링
+        mv.render(request, response);
+
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest request) {
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+
+        // 각각의 컨트롤러에게 전달할 파라미터를 담을 맵
+        Map<String, String> paramMap = new HashMap<>();
+
+        // 파라미터 요소가 더 이상 조회가 되지 않을때까지 반복을 순회
+        while (parameterNames.hasMoreElements()) {
+            String key = parameterNames.nextElement();  // key를 얻자.
+            String value = request.getParameter(key);  // key를 통해 value를 얻자
+            paramMap.put(key, value);   // key/value를 맵에 담자.
+        }
+
+        return paramMap;
+    }
+}
---
+++ src/main/java/com/spring/basic/servlet/web/frontcontroller/v4/controller/MemberListController.java
@@ -0,0 +1,27 @@
+package com.spring.basic.servlet.web.frontcontroller.v4.controller;
+
+import com.spring.basic.servlet.domain.Member;
+import com.spring.basic.servlet.repository.MemberRepositoryImpl;
+import com.spring.basic.servlet.web.frontcontroller.Model;
+import com.spring.basic.servlet.web.frontcontroller.ModelView;
+import com.spring.basic.servlet.web.frontcontroller.v3.ControllerV3;
+import com.spring.basic.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListController implements ControllerV4 {
+
+    private MemberRepositoryImpl repository = MemberRepositoryImpl.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Model model) {
+
+        List<Member> memberList
+                = repository.getList();
+
+// [삭제]        ModelView mv = new ModelView("member/members");
+        model.addAttribute("mList", memberList);
+        return "member/members";
+    }
+}
