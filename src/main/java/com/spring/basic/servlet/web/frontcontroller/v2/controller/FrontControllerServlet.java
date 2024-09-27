2acb3997052b21ac2814462cf0e848450edd414a
jh080724 <tsj010813@gmail.com>
2024-09-27 15:25:02 +0900

    FrontController v2 구축

---
+++ src/main/java/com/spring/basic/servlet/web/frontcontroller/v2/controller/FrontControllerServlet.java
@@ -0,0 +1,53 @@
+package com.spring.basic.servlet.web.frontcontroller.v2.controller;
+
+//import com.spring.basic.servlet.web.frontcontroller.v1.ControllerV1;
+import com.spring.basic.servlet.web.frontcontroller.MyViewResolver;
+import com.spring.basic.servlet.web.frontcontroller.v2.ControllerV2;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/front-controller/v2/*")
+public class FrontControllerServlet extends HttpServlet {
+
+    // 각 컨트롤러들을 모아놓을 Map생성
+    // key를 url로 주어서 각 요청마다 명령을 내릴 콘트롤러를 구분
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+
+    // 객체 초기화시 url에 따라 사용할 컨트롤러 초기화
+    public FrontControllerServlet() {
+        controllerMap.put("/front-controller/v2/member/form", new MemberFormController());
+        controllerMap.put("/front-controller/v2/member/join", new MemberJoinController());
+        controllerMap.put("/front-controller/v2/members", new MemberListController());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request,
+                           HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("[dbg] FrontControllerServlet의 service 호출!");
+
+        String requestURI = request.getRequestURI();
+
+        System.out.println("[dbg] " + requestURI);
+
+        // 요청 URL에 맞는 컨트롤러를 앱에서 꺼내오기
+        ControllerV2 controller = controllerMap.get(requestURI);
+
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // 컨트롤러 실행! (인터페이스의 구현체들이기 때문에 동일한 메서드로 호출가능)
+        MyViewResolver resolver = controller.process(request, response);
+        resolver.render(request, response);
+
+    }
+}
